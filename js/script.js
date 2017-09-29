333140f0e7b5783d3e89f46d34e7969c6a3d7a21
Nicholas D. Krotine <nkrotine18@jcu.edu>
2017-09-29 11:12:46 -0400

    added code to app.js for server shtuff

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-
-var socket = io();
-msgButton.onclick = function(e) {
-    // someone clicked send a message
-    socket.emit('message', 'hello world!');
-}
-
-
 var game = new Chess(),
 board,
 statusEl = $('#status'),
