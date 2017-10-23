c787e83b2dd604d00f0dce1e6a7e84db07749978
Nihilius <bvelicsw@gmail.com>
2017-10-23 11:21:45 -0400

    this just separates the AI and human boards, ai still not working

--- js/aiscript.js
+++ js/aiscript.js
@@ -3,7 +3,6 @@ board,
 statusEl = $('#status'),
 fenEl = $('#fen'),
 pgnEl = $('#pgn');
-
 // "rnbqkbnr/pppppppp/8/8/8/3P4/PPP1PPPP/RNBQKBNR w KQkq - 0 1"
 // setup my socket client
 // var socket = io();
@@ -13,19 +12,38 @@ pgnEl = $('#pgn');
 // }
 //NEW CODE ABOVE
 
-// do not pick up pieces if the game is over
-// only pick up pieces for the side to move
+//this version does not allow for move in draw, checkmate, or if the move 
+//from the black player
 
-  var onDragStart = function(source, piece, position, orientation) {
-  if (game.game_over() === true ||
-      (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-      (game.turn() === 'b' && piece.search(/^w/) !== -1)) 
-    {
-    return false;
-  }
+
+  var onDragStart = function (source, piece, position, orientation) {
+    if (game.in_checkmate() === true || game.in_draw() === true ||
+        piece.search(/^b/) !== -1) {
+        return false;
+    }
   }; 
 
 
+//to pick the position of the move.
+
+var makeRandomMove = function()
+{
+  if (game.turn()==='b')
+{
+  var newMove = game.moves();
+  
+   var random = [Math.floor(Math.random() * newMove.length)];
+   game.move(newMove[random]);
+
+   
+   board.position(game.fen);
+   game.turn()='w';
+}
+  updateStatus();
+};
+
+if (game.turn() ==='w')
+{
 var onDrop = function(source, target) {
 // see if the move is legal
 var move = game.move({
@@ -39,10 +57,14 @@ if (move === null) {
   return 'snapback';
 }
 
+//make a random legal move for black player
+
+  window.setInterval(makeRandomMove,250);
+
 
 updateStatus();
 }
-
+}
 // update the board position after the piece snap 
 // for castling, en passant, pawn promotion
 var onSnapEnd = function() {
