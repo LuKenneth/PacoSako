93509c91a4e9eb96c550da90f47bb92bb597db9d
LuKenneth <wannafight1@yahoo.com>
2017-11-01 09:49:32 -0400

    b and w can move union

--- js/script.js
+++ js/script.js
@@ -81,6 +81,8 @@ else {
 
 statusEl.html(status);
 fenEl.html(game.fen());
+//modified LKP: 10/31/2017 loading the fen string puts the unions in the board[]
+game.load(game.fen());
 pgnEl.html(game.pgn());
 };
 
